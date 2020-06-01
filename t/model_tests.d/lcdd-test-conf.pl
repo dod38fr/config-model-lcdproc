8d0bdf7c29744c8e21d067dd4e1639038bb022e3
Dominique Dumont <dod@debian.org>
2020-06-01 19:00:35 +0200

    reindent and cleanup of test file

--- t/model_tests.d/lcdd-test-conf.pl
+++ t/model_tests.d/lcdd-test-conf.pl
@@ -4,31 +4,34 @@ use warnings;
 my @fix_warnings ;
 
 push @fix_warnings,
-    ( 
+    (
         #load_warnings => [ qr/code check returned false/ ],
         load => "server DriverPath=/tmp/" , # just a work-around
-    ) 
+    )
     unless -d '/usr/lib/lcdproc/' ;
 
 my @tests = (
-    { # t0
-     check => {
-       'server Hello:0',           qq!"  Bienvenue"! ,
-       'server Hello:1',           qq("   LCDproc et Config::Model!") ,
-       'server Driver', 'curses',
-       'curses Size', '20x2',
-       'server AutoRotate', 'off',
-     },
-     @fix_warnings ,
-     errors => [ 
+    {
+        # t0
+        check => {
+            'server Hello:0',           qq!"  Bienvenue"! ,
+            'server Hello:1',           qq("   LCDproc et Config::Model!") ,
+            'server Driver', 'curses',
+            'curses Size', '20x2',
+            'server AutoRotate', 'off',
+        },
+        @fix_warnings ,
+        errors => [
             # qr/value 2 > max limit 0/ => 'fs:"/var/chroot/lenny-i386/dev" fs_passno=0' ,
         ],
     },
-    {   #test upgrade from raw lcdproc 0.5.5
+    {
+        # test upgrade from raw lcdproc 0.5.5
         name => 'LDCd-0.5.5',
         load_check => 'skip'
     },
-    {   # likewise for lcdproc 0.5.6
+    {
+        # likewise for lcdproc 0.5.6
         name => 'LDCd-0.5.6',
         load_check => 'skip'
     },
